435dfb020a095e21bc6c667822859c173180f282
Nguyen Tri Thanh <nguyentrithanh1311@gmail.com>
2025-04-27 00:03:33 +0700

    Update code 27/4/2025.

--- client/src/pages/Home/AddEditNotes.jsx
+++ client/src/pages/Home/AddEditNotes.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import TagInput from "../../components/Input/TagInput";
 
 const AddEditNotes = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [tags, setTags] = useState([]);
+
   return (
     <div>
       <div className="flex flex-col gap-2">
         <label className="input-label">TITLE</label>
         <input type="text"
           className="text-2xl text-slate-950 outline-none"
-          placeholder="This is title" />
+          placeholder="This is title"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }} />
       </div>
 
       <div className="flex flex-col gap-2 mt-4">
@@ -15,12 +24,17 @@ const AddEditNotes = () => {
         <textarea type="text"
           className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
           placeholder="This is content"
-          rows={10} />
+          rows={10}
+          value={content}
+          onChange={(e) => {
+            setContent(e.target.value);
+          }} />
       </div>
 
       <div className="mt-3">
         <label className="input-label">TAGS</label>
-        <TagInput />
+        <TagInput tags={tags}
+          setTags={setTags} />
       </div>
 
       <button className="btn-primary font-medium mt-5 p-3" onClick={() => {}}>
